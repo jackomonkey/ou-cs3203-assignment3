540ab6f556feddbba35418fd4d6d0770b76f600e
Ben Walls <bwallsisaboss@gmail.com>
2023-02-16 12:58:16 -0600

    first commit

---
+++ cs3203a3/src/cs3203a3/assign3.java
@@ -0,0 +1,28 @@
+package cs3203a3;
+import java.util.Scanner;
+
+public class assign3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World!");
+		
+	}
+
+	public int sumOfArray(int[] a) {
+		
+		int sum = 0;
+		
+		for(int i = 0; i < a.length; i++) {
+			
+			sum = sum + a[i];
+			
+		}
+		
+		
+		return sum;
+	}
+	
+	
+}
